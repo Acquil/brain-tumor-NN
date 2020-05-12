314b0975b4e19fb0448431279121a2707a812543
sdeepack <sdeepack@pop-os.localdomain>
2020-05-12 01:14:36 +0100

    Fixed wrong file locations

--- rescale.py
+++ rescale.py
@@ -8,7 +8,7 @@ def rescale(path,file,tumour):
     original_image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     resized_image = cv2.resize(original_image, (128, 128), 
                             interpolation=cv2.INTER_NEAREST)
-    cv2.imwrite("data-small/{0}/resized/{1}".format(tumour,file),resized_image)
+    cv2.imwrite("{0}/{1}/resized/{2}".format(data,tumour,file),resized_image)
 
 
 for root, dirs, files in os.walk('{0}/no'.format(data)):
